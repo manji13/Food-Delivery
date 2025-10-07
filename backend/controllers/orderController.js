d18641c9c99d8f5e7500c3ce5307e53b5d79358c
manjitha kavishan <manjikavi8@gmail.com>
2025-10-07 23:14:01 +0530

    add verify order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,5 +57,24 @@ const placeOrder = async (req,res) => {
     }
 }
 
+const verifyOrder = async (req,res) => {
+  const {orderId,success} = req.body;
+  try {
+    if(success=="true")
+    {
+        await orderModel.findByIdAndUpdate(orderId,{payment:true});
+        res.json({success:true,message:"Paid"})
+    }
+    else
+    {
+        await orderModel.findByIdAndDelete(orderId);
+        res.json({success:false,message:"Not paid"})
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+  }
+}
 
-export {placeOrder}
+
+export {placeOrder, verifyOrder}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPop from './components/LoginPop/LoginPop'
+import Verify from './pages/Verify/Verify'
 
 const App = () => {
 
@@ -20,7 +21,8 @@ const App = () => {
         < Route path='/' element={<Home/>} />
         < Route path='/cart' element={<Cart/>} />
         < Route path='/order' element={<PlaceOrder/>} />
-
+        < Route path='/verify' element={<Verify />} />
+        
 
       </Routes>
     </div>
